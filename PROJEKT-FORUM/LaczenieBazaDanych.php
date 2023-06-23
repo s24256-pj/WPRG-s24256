948e8d8a1aeea1f013046299fb1e4f0e073da326
s24256-pj <92204666+s24256-pj@users.noreply.github.com>
2023-06-23 10:07:48 +0200

    Update LaczenieBazaDanych.php

--- PROJEKT-FORUM/LaczenieBazaDanych.php
+++ PROJEKT-FORUM/LaczenieBazaDanych.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "forum";
+$servername = "szuflandia.pjwstk.edu.pl";
+$username = "s24256";
+$password = "Mar.Szpi";
+$dbname = "s24256";
 
 if (!mysqli_connect($servername, $username, $password, $dbname)) {
     echo "BŁĄD POŁĄCZENIA DO BAZY DANYCH";
